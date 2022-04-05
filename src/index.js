8ed08717dad6cf3aeda58966cc24bd282d324b87
Kelly Danger <kelly.danger@stitchfix.com>
2022-04-05 10:38:39 -0500

    updating notes

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// createRoot tells react where this react application (ui) should be placed in the webpage that is loaded (see index.html in the public folder)
+const root = ReactDOM.createRoot(document.getElementById('root'));//See Root in index.html
 root.render(<App />);
