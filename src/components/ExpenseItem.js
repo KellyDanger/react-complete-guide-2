841e67e9e78e6d893f4370a62f3b498d78a41c69
Kelly Danger <kelly.danger@stitchfix.com>
2022-04-05 15:07:12 -0500

    Adding data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import './ExpenseItems.css';
 
-function ExpenseItem() {
-    const dateObject = new Date(2022, 2, 28).toISOString();
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 147.00;
+function ExpenseItem(props) {
+    // const dateObject = new Date(2022, 2, 28).toISOString();
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 147.00;
 
     return (
     <div className="expense-item">
-        <div>{dateObject}</div>
+        <div>{props.date.toISOString()}</div>
         <div className="expense-item__description">
-            <h2>{expenseTitle}</h2>
-            <div className="expense-item__price">${expenseAmount}</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
         </div>
     </div>
     );
