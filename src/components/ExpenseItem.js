4a8ef22c19c800267974b68c6fa1db48294038e6
Kelly Danger <kelly.danger@stitchfix.com>
2022-04-05 11:55:48 -0500

    adding info to first component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+    <div>
+        <div>3/28/22</div>
+        <div
+            ><h2>Car Insurance</h2>
+            <div>$147.00</div>
+        </div>
+    </div>
+    );
+}
+
+export default ExpenseItem;
