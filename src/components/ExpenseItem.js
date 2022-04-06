0ee0ff8a5dfd04e9acbd8c945270ed974c7e1e56
Kelly Danger <kelly.danger@stitchfix.com>
2022-04-06 09:59:48 -0500

    Adding expense date component guts

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,15 @@
 import './ExpenseItems.css';
+import './ExpenseDate.js';
+import ExpenseDate from './ExpenseDate.js';
 
 function ExpenseItem(props) {
     // const dateObject = new Date(2022, 2, 28).toISOString();
     // const expenseTitle = 'Car Insurance';
     // const expenseAmount = 147.00;
 
-    const month = props.date.toLocaleString('en-US', {month: 'long'});
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = props.date.getFullYear();
-
     return (
     <div className="expense-item">
-        <div>
-            <div>{month}</div>
-            <div>{day}</div>
-            <div>{year}</div>
-        </div>
+        <ExpenseDate></ExpenseDate>
         <div className="expense-item__description">
             <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
