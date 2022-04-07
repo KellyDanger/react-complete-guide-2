a8a394b874fa8f555c44c3800fffc77fc63f9bc4
Kelly Danger <kelly.danger@stitchfix.com>
2022-04-07 08:39:35 -0500

    adding click event

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,9 @@ import ExpenseDate from './ExpenseDate.js';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+    const changeTitle = () =>{
+         console.log(props.id)
+    }
 
     return (
     <Card className='expense-item'>
@@ -11,6 +14,7 @@ function ExpenseItem(props) {
             <h2>{props.title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
         </div>
+        <button onClick={changeTitle}>Change Title</button>
     </Card>
     );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,12 +8,15 @@ function Expenses(props) {
   return (
     <Card className='expenses'>
       {items.map((item) => {
-        return ( <ExpenseItem
-          key={item.id}
-          title={item.title}
-          amount={item.amount}
-          date={item.date}
-        />)
+        return ( 
+          <ExpenseItem
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        )
       })}
     </Card>
   );
