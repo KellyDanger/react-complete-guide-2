30094a4aef9aee35c3c3c95fec0d6f316bb966cb
Kelly Danger <kelly.danger@stitchfix.com>
2022-04-08 08:29:57 -0500

    added handlers and listeners to form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,11 +3,18 @@ import { useState } from 'react';
 
 const ExpenseForm = () => {
     const [newTitle, setNewTitle] = useState('');
+    const [newDate, setNewDate] = useState('');
+    const [newAmount, setNewAmount] = useState('');
 
     const titleChangeHandler = (event) => {
         setNewTitle(event.target.value);
     }
-
+    const dateChangeHandler = (event) => {
+        setNewDate(event.target.value);
+    }
+    const amountChangeHandler = (event) => {
+        setNewAmount(event.target.value);
+    }
     // KELLY FOR FRIDAY....ADD HANDLERS/LISTENERS/STATE FOR THE OTHER INPUTS
 
     return (
@@ -19,11 +26,21 @@ const ExpenseForm = () => {
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31"/>
+                <input 
+                    type="date" 
+                    min="2019-01-01" 
+                    max="2022-12-31"
+                    onChange={dateChangeHandler}
+                />
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01"/>
+                <input 
+                    type="number" 
+                    min="0.01" 
+                    step="0.01"
+                    onChange={amountChangeHandler}
+                />
             </div>
         </div>
         <div className='new-expense__actions'>
