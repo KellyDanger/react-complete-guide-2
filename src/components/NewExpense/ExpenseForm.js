d53726d1d7eeb4b69d089d19fa17efcbb962ed00
Kelly Danger <kelly.danger@stitchfix.com>
2022-04-08 10:32:34 -0500

    added function prop to send data up to NewExpense

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,28 +1,60 @@
 import './ExpenseForm.css';
 import { useState } from 'react';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
+// STATE (MULTIPLE)
     const [newTitle, setNewTitle] = useState('');
     const [newDate, setNewDate] = useState('');
     const [newAmount, setNewAmount] = useState('');
 
+// STATE (SINGLE) WHEN USING THIS, YOU'LL HAVE TO USE A SPREAD OPERATOR TO MAINTAIN THE REST OF THE VALUES
+    // const [userInput, setUserInput] = useState({
+    //     newTile: '',
+    //     newAmount: '',
+    //     newDate: ''
+    // });
+
+    // EVENT HANDLERS
     const titleChangeHandler = (event) => {
         setNewTitle(event.target.value);
+// USING A SPREAD OPERATOR TO MANAGE A SINGLE STATE FOR MULTIPLE VALUES
+    //    setUserInput((prevState) => {
+    //        return {...prevState, newTitle: event.target.value };
+    // });
     }
+
     const dateChangeHandler = (event) => {
         setNewDate(event.target.value);
     }
+
     const amountChangeHandler = (event) => {
         setNewAmount(event.target.value);
     }
-    // KELLY FOR FRIDAY....ADD HANDLERS/LISTENERS/STATE FOR THE OTHER INPUTS
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData ={
+            title: newTitle,
+            amount: newAmount,
+            date: new Date(newDate)
+        };
+        props.onSaveExpenseData(expenseData);
+        console.log(expenseData);
+        setNewAmount('');
+        setNewTitle('');
+        setNewDate('');//THIS IN CONJUNCTION WITH....**
+    };
 
     return (
-    <form>
+    <form onSubmit={submitHandler}>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler}/>
+                <input 
+                    type="text" 
+                    value={newTitle} 
+                    onChange={titleChangeHandler}
+                />
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
@@ -30,6 +62,7 @@ const ExpenseForm = () => {
                     type="date" 
                     min="2019-01-01" 
                     max="2022-12-31"
+                    value={newDate} //**THIS CLEARS THE INPUT
                     onChange={dateChangeHandler}
                 />
             </div>
@@ -39,6 +72,7 @@ const ExpenseForm = () => {
                     type="number" 
                     min="0.01" 
                     step="0.01"
+                    value={newAmount}
                     onChange={amountChangeHandler}
                 />
             </div>
