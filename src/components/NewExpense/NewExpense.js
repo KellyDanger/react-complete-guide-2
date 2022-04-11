3841f9f6105c82885164ff74ecc46582f82483bf
Kelly Danger <kelly.danger@stitchfix.com>
2022-04-11 14:11:35 -0500

    formatting

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,34 +1,35 @@
-import './NewExpense.css';
-import ExpenseForm from './ExpenseForm';
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
 import { useState } from "react";
 
+const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
 
-const NewExpense = (props) => {   
-    const [showForm, setShowForm] = useState(false);
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
 
-    const saveExpenseDataHandler = (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            id: Math.random().toString()
-        };
-        props.onAddExpense(expenseData)
-    }
-    
-    const toggleForm = () => {
-        setShowForm(!showForm);
-    }
-    
-    return (
-        
-    <div className="new-expense">
-        {!showForm && <button onClick={toggleForm}>Add A New Expense Dude</button>}
-        {showForm && 
-            <ExpenseForm 
-                onSaveExpenseData={saveExpenseDataHandler}
-                toggleForm = {toggleForm}
-            />}
+  const toggleForm = () => {
+    setShowForm(!showForm);
+  };
+
+  return (
+    <div className='new-expense'>
+      {!showForm && (
+        <button onClick={toggleForm}>Add A New Expense Dude</button>
+      )}
+      {showForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          toggleForm={toggleForm}
+        />
+      )}
     </div>
-    );
-}
+  );
+};
 
 export default NewExpense;
