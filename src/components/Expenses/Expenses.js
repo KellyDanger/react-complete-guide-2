91e117817ad7013768df2b967e3a3803e9404713
Kelly Danger <kelly.danger@stitchfix.com>
2022-04-11 11:29:04 -0500

    refined filter and added error message

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,30 +2,36 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter.js";
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect } from "react";
 
 function Expenses(props) {
-  const [filterYear, setFilterYear] = useState('select');  
-  
+  const [filterYear, setFilterYear] = useState("select");
+
   // const items = props.items;
+  const filteredExpenses = props.items.filter(
+    (item) => item.date.getFullYear() === Number(filterYear)
+  );
+
+  const allExpenses = props.items;
 
   const filterChangeHandler = (selectedYear) => {
-    setFilterYear(selectedYear)//TWO WAY BINDING
-  }
-// LOGS THE VALUE OF FILTER YEAR ON CHANGE OR RELOAD OF COMPONENT
+    setFilterYear(selectedYear); //TWO WAY BINDING
+  };
+  // LOGS THE VALUE OF FILTER YEAR ON CHANGE OR RELOAD OF COMPONENT
   useEffect(() => {
     console.log(filterYear);
-  })
+  });
 
   return (
     <>
       <Card className='expenses'>
-        <ExpensesFilter selected={filterYear} onFilterChange={filterChangeHandler}/>  
-        {props.items.map((item) => {
-          let itemYear = item.date.getFullYear();
-          if (Number(filterYear) === itemYear){
-            return ( 
+        <ExpensesFilter
+          selected={filterYear}
+          onFilterChange={filterChangeHandler}
+        />
+        {/* RENDER ALL EXPENSES IF NO YEAR IS SELECTED */}
+        {filterYear === "select"
+          && allExpenses.map((item) => (
               <ExpenseItem
                 key={item.id}
                 id={item.id}
@@ -33,22 +39,21 @@ function Expenses(props) {
                 amount={item.amount}
                 date={item.date}
               />
-            )
+            ))
           }
-          if(filterYear === 'select'){
-              return ( 
-                <ExpenseItem
-                  key={item.id}
-                  id={item.id}
-                  title={item.title}
-                  amount={item.amount}
-                  date={item.date}
-                />
-            )
-          }
-          else
-            return null;
-        })};
+          {/* RENDER A MESSAGE IF NO EXPENSES EXIST IN THE FILTERED YEAR */}
+          {filteredExpenses.length === 0 && <p>You Have No Expenses</p>}
+          {/* RENDER EXPENSES FOR THE CHOSEN YEAR */}
+          {filteredExpenses.length > 0 &&
+            filteredExpenses.map((filteredItem) => (
+              <ExpenseItem
+                key={filteredItem.id}
+                id={filteredItem.id}
+                title={filteredItem.title}
+                amount={filteredItem.amount}
+                date={filteredItem.date}
+              />
+            ))}
       </Card>
     </>
   );
